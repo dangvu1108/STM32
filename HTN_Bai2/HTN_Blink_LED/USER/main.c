fafab579aaa1690f9a2b34fcb3272f5d9aa21c2f
DESKTOP-ODFNHH4\Dangvu <vudang110804@gmail.com>
2025-09-07 19:59:26 +0700

    stm32

---
+++ HTN_Bai2/HTN_Blink_LED/USER/main.c
@@ -0,0 +1,37 @@
+#include "stm32f10x_gpio.h"             // Keil::Device:StdPeriph Drivers:GPIO
+#include "stm32f10x_rcc.h"              // Keil::Device:StdPeriph Drivers:RCC
+#include "stm32f10x.h"                  // Device header
+
+void Config(){
+	GPIO_InitTypeDef gpio;
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB, ENABLE); //Bat clock cho GPIOA va GPIOB
+	
+	gpio.GPIO_Mode = GPIO_Mode_Out_PP;
+	gpio.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+	gpio.GPIO_Speed = GPIO_Speed_50MHz;
+	
+	GPIO_Init(GPIOA, &gpio);
+	GPIO_Init(GPIOB, &gpio);
+	
+}
+
+void Delay_ms(uint16_t time){
+	uint16_t i, j;
+	for(i = 0; i < time; i++){
+		for(j = 0; j < 0x2aff; j++);
+	}
+}
+int main(){
+	Config();
+	while(1){
+		// Dua dau ra cac chan set ve muc 0
+		GPIO_ResetBits(GPIOA, GPIO_Pin_0 | GPIO_Pin_1);
+		GPIO_ResetBits(GPIOB, GPIO_Pin_0 | GPIO_Pin_1);
+		Delay_ms(1000);
+		// Dua dau ra cac chan set len muc 1
+		GPIO_SetBits(GPIOB, GPIO_Pin_0 | GPIO_Pin_1);
+		GPIO_SetBits(GPIOB, GPIO_Pin_0 | GPIO_Pin_1);
+		Delay_ms(1000);
+	}
+}
+
